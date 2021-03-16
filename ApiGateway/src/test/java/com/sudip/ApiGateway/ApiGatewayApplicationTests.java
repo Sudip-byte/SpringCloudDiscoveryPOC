110f95d8f0050388e7c7cbfb2424727e20125254
Sudip Rana <sudiran1@in.ibm.com>
2021-03-17 00:11:25 +0530

    Implement API Gateway Service for Spring Cloud

---
+++ ApiGateway/src/test/java/com/sudip/ApiGateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sudip.ApiGateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
