3b13ad702735b06209a2ec7222ff3cad6ed953c5
Sudip Rana <sudiran1@in.ibm.com>
2021-03-21 16:02:07 +0530

    Implement automatic and manual routing. Also implemnt how spring cloud api load balancer works

--- usersMicroservice/src/main/java/com/sudip/users/controller/UserController.java
+++ usersMicroservice/src/main/java/com/sudip/users/controller/UserController.java
@@ -1,5 +1,7 @@
 package com.sudip.users.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,10 +10,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/users")
 public class UserController {
 	
+	@Autowired
+	private Environment env;
+	
 	@GetMapping("/status/check")
 	public String userServiceStatus()
 	{
-		return "Working";
+		return "Working on port :: " + env.getProperty("local.server.port");
 	}
 
 }
